739760a1d0048c618b4f2e191b9ca68a11046dd1
Sergey Galanov <dukalis.rak@gmail.com>
2019-01-24 00:21:07 +0200

    + locomotion smd + damping value

---
+++ Assets/Controllers/LocomotionSMB.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocomotionSMB : StateMachineBehaviour
+{
+	public float Damping;
+
+	public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		var x = Input.GetAxis("Horizontal");
+		var y = Input.GetAxis("Vertical");
+
+		animator.SetFloat("X", x, Damping, Time.deltaTime);
+		animator.SetFloat("Y", y, Damping, Time.deltaTime);
+	}
+}
