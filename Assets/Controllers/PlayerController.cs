6fdd8870675649dd41b59db3f2eb3d9bb1d0a578
Sergey Galanov <s.galanov@lucidrealitylabs.com>
2019-01-23 21:29:16 +0200

    - init comit

---
+++ Assets/Controllers/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		_animatorController = GetComponent<Animator>();
+	}
+
+	private Animator _animatorController;
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		var x = Input.GetAxis("Horizontal");
+		var y = Input.GetAxis("Vertical");
+
+		_animatorController.SetFloat("X", x);
+		_animatorController.SetFloat("Y", y);
+	}
+}
